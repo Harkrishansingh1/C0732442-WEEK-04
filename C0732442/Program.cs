0690afef6c710911ec8adfb6d2f3ec40fab14dc6
visualstudio <visualstudio@122-PC39>
2019-01-30 12:05:09 -0500

    output------mapletoonto

--- C0732442/Program.cs
+++ C0732442/Program.cs
@@ -8,6 +8,11 @@ namespace C0732442
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Lakeshore blue = new Lakeshore();
+            blue.Launch();
+        }
 
     }
 
@@ -23,11 +28,16 @@ namespace C0732442
         Village Maple = new Village();
         Village Toronto = new Village();
         Village Ajax = new Village();
+        Village temp;
         Village First;
         Village Last;
 
+
         public void Launch()
         {
+            Village temp;
+            Village First = Maple;
+            Village Last = Ajax;
             Maple.VillageName = "Maple";
             Maple.nextVillage = Toronto;
             Maple.previous = null;
@@ -37,9 +47,24 @@ namespace C0732442
             Ajax.VillageName = "Ajax";
             Ajax.nextVillage = null;
             Ajax.previous = Toronto;
-
-
-
+            Console.WriteLine(this.displayMap());
+            Console.ReadLine();
         }
+
+        public string displayMap()
+        {
+            string listOfCities = " ";
+            
+            
+          listOfCities = listOfCities + First.VillageName;
+          temp = First.nextVillage;
+          listOfCities = listOfCities + temp.VillageName;
+
+            return listOfCities;
+        }
+        
+        
+     
+        
     }
 }
